6d266e76ef21ef6e39aba49a50e6c4c46f7ef706
Hasan <hsndogn80@gmail.com>
2022-04-04 01:43:03 +0300

    Main singleton pattern

---
+++ 1-1-Singleton/Program.cs
@@ -0,0 +1,66 @@
+﻿using _1_1_Singleton;
+using _1_1_Singleton.LazySingleton;
+using _1_1_Singleton.ThreadedLazySignleton;
+using _1_1_Singleton.ThreadSafeLazySingleton;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Normal Singleton *******************************************************
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Singleton s = Singleton.Instance;
+        //    s.PrintName();
+        //}
+        //Singleton s1 = Singleton.Instance;
+        //Singleton s2 = Singleton.Instance;
+
+        //if (s1 == s2)
+        //{
+        //    Console.WriteLine("Aynı Objeler");
+        //}
+        //else
+        //    Console.WriteLine("Farklı Objeler");
+
+
+        // Lazy Singleton *************************************************************
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    LazySingleton s = LazySingleton.Instance;
+        //    s.PrintName();
+        //}
+
+        // ThreadedLazySingleton ********************************************************
+        //Program client = new Program();
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    Thread thread = new Thread(new ThreadStart(client.Run));
+        //    thread.Start();
+        //}
+
+        // Thread-Safe Singleton ****************************************
+        Program client = new Program();
+        for (int i = 0; i < 100; i++)
+        {
+            Thread thread = new Thread(new ThreadStart(client.Run));
+            thread.Start();
+        }
+
+    }
+
+    // ThreadedLazySingleton için oluşturuldu.
+    public void Run()
+    {
+        ThreadedLazySingleton ls = ThreadedLazySingleton.Instance;
+        ls.PrintName();
+    }
+
+    // ThreadSafeLazySingleton için oluşturuldu 
+    public void Start()
+    {
+        ThreadSafeLazySingleton ls = ThreadSafeLazySingleton.Instance;
+        ls.PrintName();
+    }
+}
---
+++ 1-1-Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_Singleton
+{
+    public class Singleton
+    {
+        // Değişkeni private
+        private static Singleton singleton = new Singleton();
+
+        private static int count;
+        private readonly string name;
+
+        //const private
+        private Singleton()
+        {
+            count++;
+            name = "Singleton" + count; 
+        }
+
+        // Nesnenin oluşturulacağı yer
+        public static Singleton Instance
+        {
+            get { return singleton; }
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
