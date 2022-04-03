f85b3ab611fa3063178c6aa9c5a091b0914ab63e
Hasan <hsndogn80@gmail.com>
2022-04-04 01:41:37 +0300

    Bill Pugh solution for Singleton.

---
+++ 1-1-BillPughForSignleton/BillPughSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_BillPughForSignleton
+{
+    public class BillPughSingleton
+    {
+        private static int count;
+        public string Name { get; }
+
+        private BillPughSingleton()
+        {
+            Name = "BillPughSingleton" + count++;
+        }
+
+        private static class SingletonHelper
+        {
+            public static readonly BillPughSingleton instance = new BillPughSingleton();
+        }
+
+        public void printName()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public static BillPughSingleton getInstance()
+        {
+            return SingletonHelper.instance;
+        }
+    }
+}
---
+++ 1-1-BillPughForSignleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using _1_1_BillPughForSignleton;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // THIS SOLUTION IS ONLY FOR JAVA*********************************************
+       for(int i = 0; i < 100; i++)
+        {
+            // new runBillMethod();
+        }
+    }
+
+    public void runBillMethod()
+    {
+        //BillPughSingleton ls = new BillPughSingleton.getInstance();
+        // ls.printName();
+    }
+}
